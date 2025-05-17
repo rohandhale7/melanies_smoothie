3ef119f8db2edb40a3e455a7a43c57c76c4dbfbe
rohandhale7 <65490937+rohandhale7@users.noreply.github.com>
2025-05-17 23:23:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 from snowflake.snowpark.functions import col, when_matched
 import streamlit as st
 import requests
+import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Example Streamlit App :cup_with_straw:")
