eb1c0b538715018a0324f1f19f63238bb6a1e0a5
rohandhale7 <65490937+rohandhale7@users.noreply.github.com>
2025-05-17 23:20:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ st.write("The name on your Smoothie will be", title)
 # Get Snowflake session
 cnx=st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name')).to_pandas()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'),col('Search_on'))
+st.dataframe(data=my_dataframe,use_container_width=True)
+st.stop()
 
 # Convert DataFrame column to list
 fruit_list = my_dataframe['FRUIT_NAME'].tolist()
